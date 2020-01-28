6cacf1fe341916c631258284d984b6838432c68e
dgghun <dgghun@gmail.com>
2020-01-28 08:22:46 -0800

    modified totals

--- makeFileList.sh
+++ makeFileList.sh
@@ -3,25 +3,38 @@
 # SCRIPT NAME:    makeFileList.sh
 # BY:             David Garcia
 # DATE:           
-# DESCRIPTION:                     
+# DESCRIPTION:    Creates a file list recursively from current directory
 # 
 ################################################################################
 #*******************************************************************************
-# VARIABLES ********************************************************************
+# SCRIPT START ********************************************************************
 #*******************************************************************************
-FILENAME="${1}"
 
-ls -lhRA | 
+totalFiles=`find ./ -type f 2> /dev/null| wc -l`
+totalSize=`du -sh ./ 2> /dev/null | awk '{print $1}'`
+printf "TOTAL FILES: "
+printf "%'d" "$totalFiles"
+echo ""
+echo "TOTAL SIZE: $totalSize"
+echo ""
+
+ls -lhRA --time-style="+%x %I:%M%P" | 
   sed 's/^\.\//DIRECTORY: /g' | 
   sed 's/^total/DIRECTORY SIZE: /g' |
+  grep ^l -v |
   awk '{
         if(NR <= 2)
          next
         
         if(NR == 3){
           print "TOP LEVEL DIRECTORIES"
-          printf "%-7s %-8s %-8s %-1s \n",
+          printf "%-7s %-13s %-10s %-25s \n",
                   "Size","Date","Time","Name"
+        }
+        
+        if(length($0) <= 1){
+          print ""
+          next
         }
         
         if( $1 ~ /DIRECTORY:/){
@@ -31,23 +44,24 @@ ls -lhRA |
           getline
           
           if(length($0) > 1)
-           printf "%-7s %-8s %-8s %-1s \n",
+           printf "%-7s %-13s %-10s %-25s \n",
                   "Size","Date","Time","Name"
         }
         
-        if( (substr($0,1,1) == "d") || (substr($0,1,1) == "-") ) {
+        if( (substr($0,1,1) == "l") || (substr($0,1,1) == "d") || (substr($0,1,1) == "-") ) {
           type=""
           if(substr($0,1,1) == "d")
             type="<- (Directory)"
-          printf "%-7s %-3s %-4s %-8s %-20s %-1s\n",
+          else if(substr($0,1,1) == "l")
+            type="<- (Link)"
+            
+          #if($5 ~ /^[0-9]+$/)
+           $5 = $5"B"
+           
+          printf "%-7s %-13s %-10s %-35s %-10s %-1s\n",
                   $5,$6,$7,$8,$9,type
         }
         else 
           print $0
-       }'
-totalFiles=`find ./ -type f | wc -l`
-totalSize=`du -sh ./ | awk '{print $1}'`
-echo ""
-echo "TOTAL FILES: $totalFiles"
-echo "TOTAL SIZE: $totalSize"
+       }' 2> /dev/null
 
