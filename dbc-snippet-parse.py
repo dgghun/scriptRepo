2fef13d1f123ce41a57f02b16b85fa1624a8eec6
dgghun <dgghun@gmail.com>
2021-03-02 07:19:51 -0800

    small changes

--- dbc-snippet-parse.py
+++ dbc-snippet-parse.py
@@ -2,15 +2,21 @@ import json
 
 filePath = "C:\\Users\\DGarcia\\Documents\\GitHub\\dbc-snippets\\snippets\\snippets.code-snippets"
 
-#str = f.read()
-with open(filePath) as f:
-  data = json.load(f)
+f = open(filePath, "r")
+data = json.loads(f.read())
+#with open(filePath) as f:
+#  data = json.load(f)
 
+snipArray = []
 for desc in data:
-    for key in data[desc]:
-        snippet = data[desc][key]
-        if(key == 'prefix'):
-            print('Snippet: "'+ snippet[0] +'"')
-            print('Description: ' + desc)
-            print('')
+  for key in data[desc]:
+      snippet = data[desc][key]
+      if(key == 'prefix'):
+          snipArray.append(snippet[0])
+          #print('Snippet: "'+ snippet[0] + '"' + ', Description: ' + desc)
+          #print('')
 
+tmpArray = sorted(snipArray)
+for i in tmpArray:
+  if len(i) > 0:
+    print("* <span style='color:#1E90FF;'>" + i + "</span>")
