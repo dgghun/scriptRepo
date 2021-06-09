77bcdfdce303192a79dee5e6c13d296d77df34e5
root <gabriel3052000@yahoo.com>
2021-06-09 15:17:08 -0700

    updated FULLNAME fields format. Added exception handling

--- get_db_info.py
+++ get_db_info.py
@@ -1,18 +1,27 @@
 import json
 import sys
+import re
 
-
+#..............................................................................
+# Prints out all associated databases with a program. The database and program
+# info is contained in pre-compiled JSON files defined below.
+#..............................................................................
 
 # print('Number of arguments:', len(sys.argv), 'arguments.')
 # print('Argument List:' + str(sys.argv))
 
+#..............................................................................
+# VARIABLES
+#..............................................................................
+FILE_PROGRAMS_USED = 'DBC-PROGRAM-USED.JSON'        #contains all the database names a programs uses
+FILE_DB_DEFINITIONS = 'DBC-DEFINITION-FILES.JSON'   #contains database/file definitions of a program
 #..............................................................................
 # FUNCTIONS
 #..............................................................................
 #Returns an array of all databases used in the program name passed in
 def GetProgramUsedDatabases(prgname):
     dbArray = []
-    with open('DBC-PROGRAM-USED.JSON', 'r') as infile:
+    with open(FILE_PROGRAMS_USED, 'r') as infile:
         data = json.load(infile)
         for i in data:
             for j in data[i]:
@@ -39,11 +48,13 @@ def ShowAllDefinitions(dbArray):
             print("#,\tName, \t\tType, \tSize, \tDescription")
             for field in dbDefs['Def-Field']:
                 fCnt += 1
+                fullName = str(field['Full-Name']).lower().replace(" ", "_")    #lower case & remove blanks
+                fullName = re.sub('^[0-9]\.[0-9][A-Za-z]_','',fullName)         #remove prepended version numbers
                 print(str(fCnt) + ":\t," + 
                 field['Field-Name'] + "\t," + 
                 field['Field-Type'] + "\t," +
                 field['Field-Size'] + "\t," +
-                str(field['Full-Name']).lower().replace(" ", "_")
+                fullName
                 )
 
 
@@ -51,7 +62,7 @@ def GetDatabaseDefinition(db):
     dbName = db + '.TXT'
     db1Name = db + '1.TXT'
     dbInfo = {}
-    with open('DBC-DEFINITION-FILES.JSON', 'r') as infile:
+    with open(FILE_DB_DEFINITIONS, 'r') as infile:
         data = json.load(infile)
         for i in data:
            for j in data[i]:
@@ -66,8 +77,13 @@ def GetDatabaseDefinition(db):
 # Script Start
 #..............................................................................
 
-programName = sys.argv[1]       #get passed in program name
-cdb = '1'                       
+try:
+    programName = sys.argv[1]       #get passed in program name
+except IndexError:
+    print('No program name passed in.')
+    print('Format: get_db_info.py programName')
+    quit()    
+
 
 print('ProgramName: ' + programName)
 dbs = GetProgramUsedDatabases(programName)  #get databases used in program
